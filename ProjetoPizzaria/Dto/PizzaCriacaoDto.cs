b343092f29a485fb6d079382059ffb565e89f1eb
LucasHFerreira99 <lucashf99@outlook.com.br>
2024-07-12 02:23:50 -0300

    Modificado o tema e adicionado o modificador necessario
    Foi modificado o layout e adicionado o modificador necessário nos atributos de PizzaModel e PizzaCriacaoDto

--- ProjetoPizzaria/Dto/PizzaCriacaoDto.cs
+++ ProjetoPizzaria/Dto/PizzaCriacaoDto.cs
@@ -1,10 +1,19 @@
-﻿namespace ProjetoPizzaria.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoPizzaria.Dto
 {
     public class PizzaCriacaoDto
     {
+        [Required(ErrorMessage = "Preencha o nome!")]
         public string Sabor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Você deve selecionar uma foto!")]
         public string Capa { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Preencha a descrição!")]
         public string Descricao { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Preencha a descrição!")]
         public double Valor { get; set; }
     }
 }
--- ProjetoPizzaria/Models/PizzaModel.cs
+++ ProjetoPizzaria/Models/PizzaModel.cs
@@ -1,12 +1,19 @@
-﻿using System.Security.Principal;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
 
 namespace ProjetoPizzaria.Models
 {
     public class PizzaModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Preencha o nome!")]
         public string Sabor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Você deve selecionar uma foto!")]
         public string Capa { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Preencha a descrição!")]
         public string Descricao { get; set; } = string.Empty;
         public double Valor { get; set; }
     }
